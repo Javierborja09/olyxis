9062340cd3c6b95470063c29a194f531be2967e5
Javier Jeanpool Borja Samaniego <javierborjasamaniego@gmail.com>
2026-01-24 04:38:44 -0500

    Feat: auto-create views and inject routes in MakeController

--- Framework/Console/Commands/MakeControllerCommand.php
+++ Framework/Console/Commands/MakeControllerCommand.php
@@ -6,7 +6,6 @@ use Framework\Console\Command;
 
 class MakeControllerCommand extends Command
 {
-
     public function execute(array $args)
     {
         if (empty($args[0])) {
@@ -16,12 +15,19 @@ class MakeControllerCommand extends Command
         }
 
         $name = $args[0];
-
         if (!str_ends_with($name, 'Controller')) {
             $name .= 'Controller';
         }
 
-        $content = <<<PHP
+        $controllerPath = "app/Controllers/{$name}.php";
+        if (file_exists($controllerPath)) {
+            $this->error("❌ El controlador '{$name}' ya existe.");
+            return;
+        }
+
+        $viewFolder = strtolower(str_replace('Controller', '', $name));
+
+        $controllerContent = <<<PHP
 <?php
 namespace App\Controllers;
 
@@ -30,15 +36,44 @@ use Framework\Core\Controller;
 class {$name} extends Controller {
     
     public function index(\$request) {
-        return \$this->view('home/index', [
+        return \$this->view('{$viewFolder}/index', [
             'title' => '{$name}'
         ], 'layouts/main');
     }
 }
 PHP;
 
-        $this->createFile("app/Controllers/{$name}.php", $content);
-        $this->success("\nControlador creado correctamente!");
-        $this->info("No olvides agregarlo a config/routes.php\n");
+        $viewContent = <<<HTML
+<h1>Vista de {$viewFolder}</h1>
+<p>Generada automáticamente por el comando make:controller</p>
+HTML;
+
+        $this->createFile($controllerPath, $controllerContent);
+        $this->createFile("resources/views/{$viewFolder}/index.php", $viewContent);
+
+        $this->addRouteToConfig($viewFolder, $name);
+
+        $this->success("\n✅ ¡Todo listo!");
+        $this->info("- Controlador: {$controllerPath}");
+        $this->info("- Vista: resources/views/{$viewFolder}/index.php");
+        $this->info("- Ruta: Añadida automáticamente a config/routes.php");
+    }
+
+    private function addRouteToConfig($path, $controllerName)
+    {
+        $routesPath = 'config/routes.php';
+        
+        if (!file_exists($routesPath)) return;
+
+        $content = file_get_contents($routesPath);
+
+        $newRoute = "        '/{$path}' => '{$controllerName}@index',";
+
+        $pattern = "/'GET' => \[/";
+        
+        if (preg_match($pattern, $content)) {
+            $content = preg_replace($pattern, "'GET' => [\n{$newRoute}", $content);
+            file_put_contents($routesPath, $content);
+        }
     }
 }
